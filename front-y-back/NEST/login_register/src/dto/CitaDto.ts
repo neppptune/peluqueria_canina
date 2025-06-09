9fc24ea6c3e33ca0f4682280ca3aace8595586f7
Proyectos-Hugo <proyectos.hugocambero@gmail.com>
2025-06-09 18:52:17 +0200

    Actualizado el CitaDto y actualizado el citaService con devolucion y alta

--- front-y-back/NEST/login_register/src/dto/CitaDto.ts
+++ front-y-back/NEST/login_register/src/dto/CitaDto.ts
@@ -1,23 +1,18 @@
 import { IsDate, IsInt, IsString, Length } from 'class-validator';
-export class CitaDTO {
-  id_cita?: number;
-  @IsDate()
+export class CitaDto {
+  id_cita: number;
   fecha: Date;
   hora: string;
-  @Length(4)
-  mascotaId: number;
-  @Length(9, 9)
-  empleadoDni: string;
-  @IsString()
-  @Length(50)
-  clienteEmail: string;
+  id_mascota: number;
+  dni_empleado: string;
+  email_cliente: string;
 
-  constructor(fecha: Date, hora: string, mascotaId: number, empleadoDni: string, clienteEmail: string, id_cita?: number) {
+  constructor(fecha?: Date, hora?: string, mascotaId?: number, empleadoDni?: string, clienteEmail?: string, id_cita?: number) {
     this.fecha = fecha;
     this.hora = hora;
-    this.mascotaId = mascotaId;
-    this.empleadoDni = empleadoDni;
-    this.clienteEmail = clienteEmail;
+    this.id_mascota = mascotaId;
+    this.dni_empleado = empleadoDni;
+    this.email_cliente = clienteEmail;
     this.id_cita = id_cita;
   }
 }
--- front-y-back/NEST/login_register/src/service/cita.service.ts
+++ front-y-back/NEST/login_register/src/service/cita.service.ts
@@ -1,7 +1,8 @@
 import { Cita } from './../model/Cita';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CitaDTO } from 'src/dto/CitaDto';
+import { CitaDto } from 'src/dto/CitaDto';
+import { Producto } from 'src/model/Producto';
 import { Repository } from 'typeorm';
 
 
@@ -13,17 +14,32 @@ export class CitaService {
   ){}
 
   // Devolucion de todas las citas
-  async findAllCitas(): Promise<CitaDTO[]> {
+  async findAllCitas(): Promise<CitaDto[]> {
     return await this.repositoryCita.find();
   }
 
   // Devover las citas de un cliente
-  async findQuotesByClient(email:string):Promise<CitaDTO[]>{
-    return await this.repositoryCita.find();
+  async findQuotesByClient(cita:CitaDto):Promise<CitaDto[]>{
+    const result = await this.repositoryCita.createQueryBuilder("cita")
+    .where("dni_usuario:=dni",{dni:cita.dni_empleado})
+    .getMany();
+
+    if(result){
+      return result;
+    }
   }
 
-  // Modificar una cita
-  async updateQuote(emal:string, cita:CitaDTO):Promise<CitaDTO>{
+  // Alta de una cita
+  async highQuote(cita:CitaDto):Promise<boolean>{
+    const high :Cita = await this.repositoryCita.createQueryBuilder("citas")
+    .where("id_cita=:cod",{cod:cita.id_cita})
+    .getOne();
     
+    if(high){
+      return false;
+    }else{
+      this.repositoryCita.save(high);
+      return true;
+    }
   }
 }
